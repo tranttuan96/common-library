5223497fbe2e53765d147abb3831686df4c58c2f
tuandsv <ttran@swellenergy.com>
2023-12-07 17:32:59 +0700

    Reservations CRUD

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -13,4 +13,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
