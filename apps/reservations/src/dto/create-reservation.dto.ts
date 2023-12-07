f8f44dee934e6afcaf2205d87d17dc438632f7af
tuandsv <ttran@swellenergy.com>
2023-12-07 19:47:59 +0700

    add logging and validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,8 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
-  timestamp: Date;
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
-  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
